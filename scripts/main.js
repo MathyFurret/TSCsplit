d044774a0ec27bef90af9902c4beac158ddcd2c4
Alexander B <mathfreak231@users.noreply.github.com>
2016-04-20 18:45:27 -0500

    build the list of games

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-var tsc = {};
+//build the list of games
+var games = [];
+var game_list = $.ajax('http://soniccenter.org/source/utilities/codebot.php?choice=9&site=1')
+var matches = mystring.match(/\S+?;/g);
+for (var i in matches) {
+	var match = game_list[i].match(/(\d+)-([^;]+)/);
+	games.push({'id': match[1], 'name': match[2]})
+}
